1e556e37bdad2666ae112bc12d9c1f9eda9a973e
Amit Anand <anand.amit943@gmail.com>
2017-12-18 20:38:26 -0800

    implementation with cmd

--- nextbus.py
+++ nextbus.py
@@ -1,9 +1,19 @@
 import urllib.request
 from xml.etree.ElementTree import XML
+import sys
 
-u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14787&route=22')
+if len(sys.argv) != 3:
+    raise SystemExit('Usage: nextbus.py route stopid')
+
+route = sys.argv[1]
+stopid = sys.argv[2]
+
+# print("Command options :",sys.argv)     #shows the argument is sys.argv
+# raise SystemExit(0)
+
+u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop={}&route={}'.format(stopid,route))
 data = u.read()
-print(data)
+# print(data)
 doc = XML(data)
 for i in doc.findall('.//pt'):
     print(i.text)
